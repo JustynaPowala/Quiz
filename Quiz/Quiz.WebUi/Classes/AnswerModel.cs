89b820ca6251416f37817e4eb1a5cde02df61f14
JustynaPowala <justyna.powala96@gmail.com>
2023-04-28 15:10:15 +0200

    proper paging

---
+++ Quiz/Quiz.WebUi/Classes/AnswerModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Quiz.WebUi.Classes
+{
+    public class AnswerModel
+    {
+        public AnswerModel() { }
+        
+            [Required]
+            public string? Answer { get; set; }
+            public bool IsCorrect { get; set; }
+
+        
+    }
+}
---
+++ Quiz/Quiz.WebUi/Classes/QuestionModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Quiz.WebUi.Classes
+{
+    public class QuestionModel
+    {
+        public QuestionModel() { }
+
+
+            [Required]
+            public string? Question { get; set; }
+
+            [Required]
+            public int Points { get; set; }
+
+            [Required]
+            public string? Category { get; set; }
+
+            [Required]
+            public string? SelectionMultiplicity { get; set; }
+        
+        
+
+    }
+}
