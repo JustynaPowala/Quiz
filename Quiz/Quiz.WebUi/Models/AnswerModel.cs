08bf424a6ca7b3ed226ae8f5788b120f5d1fcfd1
JustynaPowala <justyna.powala96@gmail.com>
2023-05-01 21:45:40 +0200

    The ability to set correct answers depending on the question - whether it is single or multiple choice

--- Quiz/Quiz.WebUi/Models/AnswerModel.cs
+++ Quiz/Quiz.WebUi/Models/AnswerModel.cs
@@ -1,15 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Quiz.WebUi.Models
 {
     public class AnswerModel
     {
         public AnswerModel() { }
-        
-            [Required]
-            public string? Answer { get; set; }
-            public bool IsCorrect { get; set; }
 
-        
+        [Required]
+        public string? Answer { get; set; }
+        public bool IsCorrect { get; set; }
+
     }
 }
